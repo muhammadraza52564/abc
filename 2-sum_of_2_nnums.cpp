02f24dcab4d54d9cef18c81e81d0025c78be40bd
muhammadraza52564 <muhammadraza52564@gmail.com>
2023-10-23 21:22:15 +0500

    Mine

---
+++ 2-sum_of_2_nnums.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int num1,num2,sum;
+ cout<<"Enter 1st number";
+ cin>>num1;
+ cout<<"Enter 2nd number";
+ cin>>num2;
+ cout<<"Sum of " <<num1<<" and " <<num2 <<" = "<<sum;
+ return 0;
+}
+
---
+++ 3-value_of_datatypes_bytes.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ cout<<" The sizeof(char) is :"<<sizeof(char) <<endl;
+ cout<<"The sizeof(short) is : "<<sizeof(short)<<endl;
+ cout<<"The sizeof(int) is : "<<sizeof(int)<<endl;
+ cout<<"The sizeof(long) is : "<<sizeof(long)<<endl;
+ cout<<"The sizeof(long integer) is :"<<sizeof(long long)<<endl;
+ cout<<"The sizeof(float) is : "<<sizeof(float)<<endl;
+ cout<<"The sizeof(double) is : "<<sizeof(double)<<endl;
+ cout<<"The sizeof(long double) is : "<<sizeof(long double)<<endl;
+ cout<<"The sizeof(bool) is : "<<sizeof(bool)<<endl;
+ return 0;
+}
+
